2562436d51a6c85acb25c542d09a7d88612c3693
Donalsien Akow <donalsienkum@gmail.com>
2026-07-08 10:10:24 +0100

    Add analytics, cloud skills, update OG URL to donalsien.vercel.app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Skills from "./components/Skills";
 import Writing from "./components/Writing";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </>
   );
 }
